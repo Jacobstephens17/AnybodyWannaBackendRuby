5626cdc9710bfc230bde3d6d676ae6b011baddcd
Jacob Stephens <jacobstephens2@gmail.com>
2021-06-12 19:02:12 -0700

    added posts table and controller, need to add crud operations to both tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_12_212207) do
+ActiveRecord::Schema.define(version: 2021_06_13_014126) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.string "location"
+    t.string "group"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,11 @@
-User.create({ username: '@jacobstephens', password: 'testPassword' })
+User.create({ 
+    username: '@jacobstephens', 
+    password: 'testPassword' 
+})
+
+Post.create({
+    title: 'Game Night',
+    description: 'We will be playing COD',
+    location: 'Bens House',
+    group: 'Gamerz'
+})
